4bf340101294a2d198ed98e99bfa1bf6b26216b9
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-13 15:37:53 +0300

    add quiz entity model

---
+++ lib/features/student/quizzes/data/models/quiz_data_model.dart
@@ -0,0 +1,73 @@
+import 'package:universityhup/features/student/quizzes/data/models/question_data_model.dart';
+import 'package:universityhup/features/student/quizzes/domain/entities/quiz_data_entity.dart';
+
+class QuizDataModel extends QuizDataEntity{
+  QuizDataModel(
+      {required super.id,
+       required super.title,
+       required super.startDate,
+       required super.endDate,
+       required super.grade,
+       required super.courseId,
+       required super.instructorId,
+       required super.createdAt,
+       required super.questions});
+
+  factory QuizDataModel.fromJson(Map<String, dynamic> json) {
+    return QuizDataModel(
+    id : json['id'],
+    title : json['title'],
+    startDate : json['startDate'],
+    endDate : json['endDate'],
+    grade : json['grade'],
+    courseId : json['courseId'],
+    instructorId : json['instructorId'],
+    createdAt : json['createdAt'],
+    questions: fillQuestions(json),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = id;
+    data['title'] = title;
+    data['startDate'] = startDate;
+    data['endDate'] = endDate;
+    data['grade'] = grade;
+    data['courseId'] = courseId;
+    data['instructorId'] = instructorId;
+    data['createdAt'] = createdAt;
+    if (questions != null) {
+      data['questions'] =questions!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+
+}
+
+class SubmitQuizModel {
+  bool? q0011;
+
+  SubmitQuizModel({this.q0011});
+
+  SubmitQuizModel.fromJson(Map<String, dynamic> json) {
+  q0011 = json['Q001_1'];
+  }
+
+  Map<String, dynamic> toJson() {
+  final Map<String, dynamic> data = new Map<String, dynamic>();
+  data['Q001_1'] = this.q0011;
+  return data;
+  }
+
+}
+
+List<Questions>fillQuestions(json){
+  List<Questions>questions=[];
+ if (json['questions'] != null) {
+      json['questions'].forEach((v) {
+        questions.add(Questions.fromJson(v));
+      });
+    }
+    return questions;
+}
