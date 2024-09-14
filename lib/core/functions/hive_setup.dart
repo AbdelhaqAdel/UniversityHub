9bee4f06e572bc183f2ea18d96df829d4f1a6d7d
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-14 07:13:10 +0300

    add file entity

--- lib/core/functions/hive_setup.dart
+++ lib/core/functions/hive_setup.dart
@@ -1,6 +1,8 @@
 import 'package:hive_flutter/adapters.dart';
 import 'package:universityhup/core/constants/hive_constants.dart';
 import 'package:universityhup/features/courses/data/models/all_courses_model.dart';
+import 'package:universityhup/features/student/material/domain/entities/material_file_entity.dart';
+import 'package:universityhup/features/student/material/domain/entities/material_folder_entity.dart';
 
 import '../constants/constant.dart';
 
@@ -8,6 +10,9 @@ import '../constants/constant.dart';
 Future<void> hiveSetup() async {
   await Hive.initFlutter();
 Hive.registerAdapter(CoursesModelAdapter());
+Hive.registerAdapter(FolderEntityAdapter());
+Hive.registerAdapter(FileEntityAdapter());
+
 
 
   await Hive.openBox(HiveConstants.kStartBox);
