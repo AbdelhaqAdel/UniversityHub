4c3568bd462e48ad1835f575eb71544cf72ba47c
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-09-14 20:09:25 +0300

    grade ui

---
+++ lib/features/student_role/grade/presentation/widgets/grade_table.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GradeTable extends StatelessWidget {
+  const GradeTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Container(color: Colors.blue,));
+  }
+}
