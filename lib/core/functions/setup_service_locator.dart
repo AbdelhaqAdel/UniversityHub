01b1f9c1e4811f2384023868d59ca72f4f7dcd17
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-09-14 20:20:19 +0300

    grade service locator

--- lib/core/functions/setup_service_locator.dart
+++ lib/core/functions/setup_service_locator.dart
@@ -9,6 +9,9 @@ import '../../features/student_role/assignment/data/data_sources/assignment_remo
 import '../../features/student_role/assignment/data/repositories/assignment_repo_impl.dart';
 import '../../features/student_role/assignment/domain/repositories/assignment_repo.dart';
 import '../../features/student_role/assignment/domain/use_cases/get_assignment_usecase.dart';
+import '../../features/student_role/grade/data/data_sources/grade_remote_data_source.dart';
+import '../../features/student_role/grade/data/repositories/grade_repo_impl.dart';
+import '../../features/student_role/grade/domain/repositories/grade_repo.dart';
 
 final getIt = GetIt.instance;
 
@@ -24,6 +27,11 @@ void setupServiceLocator() {
   getIt.registerSingleton<AssignmentRepo>(
     AssignmentRepoImpl(
       assignmentRemoteDataSource: AssignmentRemoteDataSourceImpl(),
+    ),
+  );
+  getIt.registerSingleton<GradeRepo>(
+    GradeRepoImpl(
+       gradeRemoteDataSource: GradeRemoteDataSourceImpl(),
     ),
   );
 }
