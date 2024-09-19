26c5cbb7ba1e1ed13b983fdd52438de0a51d3a0b
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-19 06:25:58 +0300

    refactor quiz result UI screen

---
+++ lib/features/student_role/quizzes/presentation/widgets/quez result/quiz_result_builder.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:universityhup/features/student_role/quizzes/presentation/manager/cubit/quiz_cubit.dart';
+import 'package:universityhup/features/student_role/quizzes/presentation/widgets/quez%20result/quiz_result_widget.dart';
+
+class QuizResultBuilder extends StatelessWidget {
+   const QuizResultBuilder({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder <QuizCubit,QuizState>(builder:(context,state){
+      if(state is SubmitQuizSuccessState){
+        return QuizResultWidget(quizGrade: state.quizGrade,);
+        }else if(state is SubmitQuizErrorState){
+       return Text(state.error); 
+      }else {
+        return const Center(child: CircularProgressIndicator(),);
+      }
+    });
+  }
+}
+
+ 
