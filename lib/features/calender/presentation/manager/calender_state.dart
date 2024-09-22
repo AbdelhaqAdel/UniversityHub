8c5f4c5b6842f95429b5fc0adee2237366792c4d
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-22 04:49:19 +0300

    add calendar cubit

---
+++ lib/features/calender/presentation/manager/calender_state.dart
@@ -0,0 +1,36 @@
+import 'package:universityhup/features/calender/domain/entities/calender_entity.dart';
+
+abstract class CalendarState {}
+
+class CalendarInitialState extends CalendarState {}
+
+class AddEventLoadingState extends CalendarState {}
+class AddEventSuccessState extends CalendarState {
+  
+}
+class AddEventErrorState extends CalendarState {
+  final String error;
+  AddEventErrorState(this.error);
+}
+
+class GetEventsLoadingState extends CalendarState {}
+class GetEventsSuccessState extends CalendarState {
+  final List<CalendarEntity> events;
+  GetEventsSuccessState({required this.events});
+}
+class GetEventsErrorState extends CalendarState {
+  final String message;
+  GetEventsErrorState(this.message);
+}
+
+class ChangeCalenderDayState extends CalendarState {
+  static DateTime thisFocusDay = DateTime.now();
+  static void setFocusDay({required DateTime focusDay}){
+     thisFocusDay=focusDay;  
+  }
+  ChangeCalenderDayState(); 
+}
+
+class ChangeCalenderFormatState extends CalendarState {}
+
+class ChangeCalenderIndexState extends CalendarState {}
