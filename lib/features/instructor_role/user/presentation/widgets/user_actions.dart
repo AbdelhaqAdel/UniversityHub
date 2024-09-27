ffe35cda981c2b9da4fd6ec077dd0a0f3ebf2651
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-09-27 15:49:37 +0300

    user presentation

---
+++ lib/features/instructor_role/user/presentation/widgets/user_actions.dart
@@ -0,0 +1,337 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/widgets/glass_box.dart';
+import '../../domain/entities/user_entity.dart';
+
+class ButtonsActions extends StatelessWidget {
+  const ButtonsActions({
+    super.key,
+    required this.userEntity,
+  });
+
+  final UserEntity? userEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: InkWell(
+              onTap: () {
+              },
+              child: GlassBox(
+                widget: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Edit account',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                color: Colors.blueGrey.withOpacity(.15),
+                borderRadius: 10,
+                x: 30,
+                y: 30, border: false,),
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          Expanded(
+            child: GlassBox(
+              widget: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: InkWell(
+                  onTap: () => showDialog<String>(
+                    context: context,
+                    barrierColor: Colors.black.withOpacity(.3),
+                    useSafeArea: true,
+                    builder: (BuildContext context) => AlertDialog(
+                      insetPadding: const EdgeInsets.all(0.0),
+                      scrollable: false,
+                      shadowColor: Colors.transparent,
+
+                      // title: const Text('AlertDialog Title'),
+                      content: Center(
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: GlassBox(
+                              widget: Stack(
+                                children: [
+                                  Column(
+                                    children: [
+                                      const Spacer(),
+                                      Padding(
+                                        padding:
+                                        const EdgeInsets.all(
+                                            15.0),
+                                        child:
+                                        GlassBox(
+                                          widget: Padding(
+                                            padding:
+                                            const EdgeInsets
+                                                .all(15.0),
+                                            child: Column(
+                                              children: [
+                                                Text(
+                                                  userEntity!.facultyName!,
+                                                  style:
+                                                  const TextStyle(
+                                                    fontWeight:
+                                                    FontWeight
+                                                        .w900,
+                                                    fontSize: 15,
+                                                    color:Colors.black,
+                                                  ),
+                                                ),
+                                                const SizedBox(
+                                                  height: 10,
+                                                ),
+                                                Row(
+                                                  crossAxisAlignment:
+                                                  CrossAxisAlignment
+                                                      .center,
+                                                  mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .start,
+                                                  children: [
+                                                    _CardImage(),
+                                                    const SizedBox(
+                                                      width: 15,
+                                                    ),
+                                                    _CardInfo(userEntity: userEntity),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          color: Colors.white
+                                              .withOpacity(.5),
+                                          borderRadius: 20,
+                                          x: 18,
+                                          y: 18,
+                                          border: true,
+                                        ),
+                                      ),
+                                      const Spacer(
+                                        flex: 1,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              color: Colors.transparent,
+                              borderRadius: 0,
+                              x: 5,
+                              y: 5, border: false,),
+                          ),
+                        ),
+                      ),
+                      elevation: 0,
+                      clipBehavior: Clip.none,
+                      surfaceTintColor: Colors.transparent,
+                      backgroundColor: Colors.transparent,
+                      contentPadding: EdgeInsets.zero,
+                    ),
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Show your card',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color:Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              color: Colors.blueGrey.withOpacity(.15),
+              borderRadius: 10,
+              x: 30,
+              y: 30, border: false,),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardInfo extends StatelessWidget {
+  const _CardInfo({
+    super.key,
+    required this.userEntity,
+  });
+
+  final UserEntity? userEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment:
+      CrossAxisAlignment
+          .start,
+      children: [
+        Row(
+          children: [
+            const Text(
+              'Name : ',
+              style:
+              TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 14,
+                color: Colors.teal,
+              ),
+            ),
+            SizedBox(
+              width:
+              130,
+              child:
+              Text(
+                userEntity!.fullName!,
+                maxLines: 1,
+                style: const TextStyle(
+                  overflow: TextOverflow.ellipsis,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  color:Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height:
+          4,
+        ),
+        Row(
+          children: [
+            const Text(
+              'Academic ID : ',
+              style:
+              TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 14,
+                color: Colors.teal,
+              ),
+            ),
+            Text(
+              userEntity!.academicId!,
+              style:
+              const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+                color:Colors.black,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height:
+          4,
+        ),
+        Row(
+          children: [
+            const Text(
+              'Level : ',
+              style:
+              TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 14,
+                color: Colors.teal,
+              ),
+            ),
+            Text(
+              '${userEntity!.level!} ${userEntity!.departmentName!}',
+              style:
+              const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+                color:Colors.black,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height:
+          4,
+        ),
+        Row(
+          children: [
+            const Text(
+              'Number : ',
+              style:
+              TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 14,
+                color: Colors.teal,
+              ),
+            ),
+            Text(
+              userEntity!.phone!,
+              maxLines:
+              1,
+              style:
+              const TextStyle(
+                overflow: TextOverflow.ellipsis,
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+                color:Colors.black,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height:
+          4,
+        ),
+      ],
+    );
+  }
+}
+
+class _CardImage extends StatelessWidget {
+  const _CardImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          shape: BoxShape
+              .circle,
+          boxShadow: [
+            BoxShadow(
+              color:
+              Colors.black,
+              blurRadius:
+              0,
+              spreadRadius:
+              1,
+            ),
+          ]),
+      child:
+      const CircleAvatar(
+        radius:
+        50,
+        backgroundImage:
+        AssetImage(
+          'assets/images/profile.png',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/instructor_role/user/presentation/widgets/user_body.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:universityhup/features/student_role/user/presentation/widgets/user_actions.dart';
+import 'package:universityhup/features/student_role/user/presentation/widgets/user_image.dart';
+import 'package:universityhup/features/student_role/user/presentation/widgets/user_name.dart';
+import '../../domain/entities/user_entity.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../core/functions/custom_snack_bar.dart';
+
+import '../manager/user_cubit.dart';
+import '../manager/user_state.dart';
+import 'is_verify.dart';
+
+class UserBody extends StatefulWidget {
+  const UserBody({
+    super.key,
+  });
+
+  @override
+  State<UserBody> createState() => _UserBodyState();
+}
+
+class _UserBodyState extends State<UserBody> {
+  UserEntity? userEntity ;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<UserCubit, UserState>(
+      listener: (context, state) {
+        if(state is GetUserSuccessState){
+          showSnackBar(context: context, message: 'GetUserSuccessState');
+          userEntity=state.userEntity;
+        }
+      },
+      builder: (context, state) {
+        if(userEntity!=null ){
+          return  Column(
+            children: [
+              const ProfileImage(),
+              const SizedBox(
+                height: 3,
+              ),
+              UserName(userEntity: userEntity),
+              ButtonsActions(userEntity: userEntity),
+              const IsVerify(),
+            ],
+          );
+
+        }else{
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+
+  }
+}
+
+
+
+
+
