8d30672ac5f9f897f295036fda8f91c59adb0749
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-09-27 16:43:06 +0300

    refactor user entity

--- lib/features/instructor_role/user/domain/entities/user_entity.dart
+++ lib/features/instructor_role/user/domain/entities/user_entity.dart
@@ -1,24 +1,17 @@
 class UserInstructorEntity {
-  final String? userId;
   final String? fullName;
   final String? email;
   final String? phone;
   final String? imagePath;
-  final String? academicId;
-  final int? level;
-  final String? departmentName;
   final String? facultyName;
   final String? universityName;
 
   UserInstructorEntity(
-      {required this.userId,
+      {
       required this.fullName,
       required this.email,
       required this.phone,
       required this.imagePath,
-      required this.academicId,
-      required this.level,
-      required this.departmentName,
       required this.facultyName,
       required this.universityName});
 }
