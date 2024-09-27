91690012812edd3578b78a8d561b631af72ebae5
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-09-27 17:04:50 +0300

    refactor assignment model

--- lib/features/instructor_role/assignment/data/models/assignment_model.dart
+++ lib/features/instructor_role/assignment/data/models/assignment_model.dart
@@ -4,40 +4,31 @@ class AssignmentInstructorModel extends AssignmentInstructorEntity {
   AssignmentInstructorModel(
       {required super.taskId,
       required super.taskName,
-      required super.taskGrade,
       required super.startDate,
       required super.endDate,
-      required super.status,
-      required super.courseName,
       required super.filePath,
-      required super.instructorName,
-      required super.createdAt});
+      required super.numberOfAllStudents,
+      required super.numberOfStudentsUploads});
 
   factory AssignmentInstructorModel.fromJson(Map<String, dynamic> json) =>
       AssignmentInstructorModel(
           taskId: json['taskId'],
           taskName: json['taskName'],
-          taskGrade: json['taskGrade'],
           startDate: json['startDate'],
           endDate: json['endDate'],
-          status: json['status'],
-          courseName: json['courseName'],
           filePath: json['filePath'],
-          instructorName: json['instructorName'],
-          createdAt: json['createdAt']);
+          numberOfStudentsUploads: json['numberOfStudentsUploads'],
+          numberOfAllStudents: json['numberOfAllStudents']);
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['taskId'] = super.taskId;
     data['taskName'] = super.taskName;
-    data['taskGrade'] = super.taskGrade;
     data['startDate'] = super.startDate;
     data['endDate'] = super.endDate;
-    data['status'] = super.status;
-    data['courseName'] = super.courseName;
     data['filePath'] = super.filePath;
-    data['instructorName'] = super.instructorName;
-    data['createdAt'] = super.createdAt;
+    data['numberOfStudentsUploads'] = super.numberOfStudentsUploads;
+    data['numberOfAllStudents'] = super.numberOfAllStudents;
     return data;
   }
 }
