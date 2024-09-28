daaa86bccaffc96cab6bd151b1586e1e0f234100
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-28 19:46:35 +0300

    refactor file widgets

--- lib/features/instructor_role/material/presentation/widgets/file widgets/file_grid_view.dart
+++ lib/features/instructor_role/material/presentation/widgets/file widgets/file_grid_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:universityhup/features/student_role/material/domain/entities/material_file_entity.dart';
-import 'package:universityhup/features/student_role/material/presentation/manager/cubit/material_cubit.dart';
+import 'package:universityhup/features/instructor_role/material/domain/entities/material_file_entity.dart';
+import 'package:universityhup/features/instructor_role/material/presentation/manager/cubit/material_cubit.dart';
+
 
 import 'file_custom_widget.dart';
 
@@ -21,13 +22,13 @@ class FileGridView extends StatelessWidget {
                 crossAxisCount: 2,
                 mainAxisSpacing: 10.w,
                 crossAxisSpacing: 10.h,
-                childAspectRatio: .96,
+                childAspectRatio: .90,
               ),
               itemCount:files.length,
               physics: const BouncingScrollPhysics(),
               itemBuilder: (context, index) => InkWell(
              onTap: (){
-              MaterialCubit.get(context).openFile(filePath: files[index].filePath); 
+              InsMaterialCubit.get(context).openFile(filePath: files[index].filePath); 
              },
              child: FileWidget(
               file: files[index],
