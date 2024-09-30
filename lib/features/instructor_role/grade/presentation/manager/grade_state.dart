950129129fdf410a43a4faaf8b7a0c579a6cae06
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-30 09:21:53 +0300

    refactor grades cubit

--- lib/features/instructor_role/grade/presentation/manager/grade_state.dart
+++ lib/features/instructor_role/grade/presentation/manager/grade_state.dart
@@ -1,16 +1,36 @@
 
+import 'package:universityhup/features/instructor_role/grade/domain/entities/student_entity.dart';
+
 import '../../domain/entities/grade_entity.dart';
 
-sealed class GradeState {}
+sealed class InsGradeState {}
 
-final class GradeInitial extends GradeState {}
+final class GradeInitial extends InsGradeState {}
 
-class GetGradeLoadingState extends GradeState{}
-class GetGradeSuccessState extends GradeState{
-  final List<GradeEntity>  gradeEntity;
+class GetGradeLoadingState extends InsGradeState{}
+class GetGradeSuccessState extends InsGradeState{
+  static double totalGrade=0;
+  static setTotalGrade(double total){
+     totalGrade=total;
+  }
+  final List<InsGradeEntity>  gradeEntity;
   GetGradeSuccessState({required this.gradeEntity});
 }
-class GetGradeErrorState extends GradeState{
+class GetGradeErrorState extends InsGradeState{
   final String errMessage;
   GetGradeErrorState(this.errMessage);
+}
+
+
+
+class GetStudentForCourseLoadingState extends InsGradeState{}
+class GetStudentForCourseSuccessState extends InsGradeState{
+  static List<StudentEntity>  allStudents=[];
+  static setAllStudents(List<StudentEntity> students){
+    allStudents=students;
+  }
+}
+class GetStudentForCourseErrorState extends InsGradeState{
+  final String errMessage;
+  GetStudentForCourseErrorState(this.errMessage);
 }
