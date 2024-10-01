15c946fe4180e2aa6de31e1e93d7a75bdf5fb3eb
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-10-01 16:50:10 +0300

    quiz data source

--- lib/features/instructor_role/quizzes/domain/repositories/quiz_data_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:universityhup/core/errors/failure.dart';
-
-import '../../data/models/question_data_model.dart';
-
-abstract class QuizDataRepo{
- Future<Either<Failure, List<Questions>>>getQuizData({required String  quizId});
-}
