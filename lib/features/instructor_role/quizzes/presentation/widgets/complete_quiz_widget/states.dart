29ad31a9252ef372994b9741121f06ae1bcd3f77
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-10-01 16:56:39 +0300

    pending quiz widgets

---
+++ lib/features/instructor_role/quizzes/presentation/widgets/complete_quiz_widget/states.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class CompleteQuizStates extends StatelessWidget {
+  const CompleteQuizStates({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 45,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.teal.withOpacity(.6),
+              borderRadius: BorderRadius.circular(12),
+              // border: Border.all(width: .5),
+            ),
+            child: const Center(
+              child: Text(
+                'Completed',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
