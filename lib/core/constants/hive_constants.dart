8f203c64e5af927ee794034456f45170103d3509
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-10-07 01:08:18 +0300

    refactor hive & service locator

--- lib/core/constants/hive_constants.dart
+++ lib/core/constants/hive_constants.dart
@@ -3,5 +3,8 @@ abstract class HiveConstants {
 static const String kStartBox='start_box';
 static const String coursesBox='courses_box';
 static const String materialBox='material_box';
+static const String kUserInstructorBox='user_ins_box';
+static const String kUserStudentBox='user_stu_box';
+static const String kLoginEntityBox='login_entity_box';
 
 }
--- lib/core/functions/hive_setup.dart
+++ lib/core/functions/hive_setup.dart
@@ -2,26 +2,33 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:universityhup/core/constants/hive_constants.dart';
 import 'package:universityhup/features/courses/data/models/all_courses_model.dart';
 
+import '../../features/instructor_role/user/domain/entities/user_entity.dart';
+import '../../features/login/domain/entities/login_entity.dart';
 import '../../features/student_role/material/domain/entities/material_file_entity.dart';
 import '../../features/student_role/material/domain/entities/material_folder_entity.dart';
+import '../../features/student_role/user/domain/entities/user_entity.dart';
 import '../constants/constant.dart';
-
 
 Future<void> hiveSetup() async {
   await Hive.initFlutter();
-Hive.registerAdapter(CoursesModelAdapter());
-Hive.registerAdapter(FolderEntityAdapter());
-Hive.registerAdapter(FileEntityAdapter());
-
-
+  Hive.registerAdapter(CoursesModelAdapter());
+  Hive.registerAdapter(FolderEntityAdapter());
+  Hive.registerAdapter(FileEntityAdapter());
+  Hive.registerAdapter(UserEntityAdapter());
+  Hive.registerAdapter(UserInstructorEntityAdapter());
+  Hive.registerAdapter(LoginEntityAdapter());
 
   await Hive.openBox(HiveConstants.kStartBox);
   await Hive.openBox(HiveConstants.coursesBox);
   await Hive.openBox<FolderEntity>(HiveConstants.materialBox);
+  await Hive.openBox<UserEntity>(HiveConstants.kUserStudentBox);
+  await Hive.openBox<UserInstructorEntity>(HiveConstants.kUserInstructorBox);
+  await Hive.openBox<LoginEntity>(HiveConstants.kLoginEntityBox);
 
- 
   var startBoxData = Hive.box(HiveConstants.kStartBox);
   isOnboarding = startBoxData.get('isOnboarding') ?? false;
 
+  var box = Hive.box<LoginEntity>(HiveConstants.kLoginEntityBox);
+  loginEntity = box.get('loginEntity');
 
 }
