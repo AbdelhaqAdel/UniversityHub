f7f1a26ed1710e2f7c37b41699f4186a1355d751
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-10-10 04:42:35 +0300

    refactor layout cubit to add history

--- lib/features/layout/presentation/manager/layout_cubit/layout_state.dart
+++ lib/features/layout/presentation/manager/layout_cubit/layout_state.dart
@@ -6,3 +6,10 @@ abstract class LayoutState {}
 class LayoutInitial extends LayoutState {}
 
 class NavigationBarState extends LayoutState {}
+class GetStoryLoadingState extends LayoutState {}
+class GetStorySuccessState extends LayoutState {}
+class GetStoryErrorState extends LayoutState {}
+
+class DeleteHiStoryLoadingState extends LayoutState {}
+class DeleteHiStorySuccessState extends LayoutState {}
+class DeleteHiStoryErrorState extends LayoutState {}
