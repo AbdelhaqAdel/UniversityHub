3ee3b7fbe9ed19eb34c971eb467f89628c4890ce
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2024-10-18 18:57:20 +0300

    refacor

--- lib/features/student_role/quizzes/presentation/widgets/quez_result/quiz_result_builder.dart
+++ lib/features/student_role/quizzes/presentation/widgets/quez_result/quiz_result_builder.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:universityhup/features/student_role/quizzes/presentation/manager/cubit/quiz_cubit.dart';
-import 'package:universityhup/features/student_role/quizzes/presentation/widgets/quez%20result/quiz_result_widget.dart';
+import 'package:universityhup/features/student_role/quizzes/presentation/widgets/quez_result/quiz_result_widget.dart';
 
 class QuizResultBuilder extends StatelessWidget {
    const QuizResultBuilder({super.key});
